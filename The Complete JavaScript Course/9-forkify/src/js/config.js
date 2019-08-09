803410f68bf0d14e69a3645e756b2bee496c2b1f
RIProG <riprog@hotmail.com>
2019-08-09 14:59:11 +0200

    Course Complete!

--- The Complete JavaScript Course/9-forkify/src/js/config.js
+++ The Complete JavaScript Course/9-forkify/src/js/config.js
@@ -1,4 +1,7 @@
 export const proxy = 'http://cors-anywhere.herokuapp.com/';
 //export const key = 'c240889fc3956cfb2276b0d266ef065e';  // Original email
-//export const key = '2cffd9139dfa493bd44115c021e8059d'; // 10 minute
-export const key = 'e9aab48e1a0d6b023622c9804f02381b'; // 10 minute
+//export const key = '2cffd9139dfa493bd44115c021e8059d'; // 10 minute mail
+//export const key = 'e9aab48e1a0d6b023622c9804f02381b'; // 10 minute mail
+//export const key = '56ba3600010aa74fde7b030f4d7981cf'; // 10 minute mail
+//export const key = 'ca9096d0e9aedfd4a9be6e717e77060a'; // 10 minute mail
+export const key = 'fa2ebe2df635d6baaaac81ff5dcfd294'; // 10 minute mail
