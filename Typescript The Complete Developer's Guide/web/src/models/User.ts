7c6a50187d79091a794311353ef6b43ca606054b
RIProG <riprog@hotmail.com>
2019-08-16 16:26:06 +0200

    Section 13 almost complete

--- Typescript The Complete Developer's Guide/web/src/models/User.ts
+++ Typescript The Complete Developer's Guide/web/src/models/User.ts
@@ -1,23 +1,33 @@
-interface UserProps {
+import { Model } from './Model';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
+import { Collection } from './Collection';
+
+export interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
 
-type Callback = () => {}
+const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  constructor(private data: UserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName];
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
+  }
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
+    );
   }
 
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  setRandomAge(): void {
+    const age = Math.round(Math.random() * 100);
+    this.set({ age });
   }
-
-  on(eventName: string, callback: Callback) {
-
-  }
-
 }
