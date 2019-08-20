a84e2f65953434732e282f90fb0a119e4365c99b
RIProG <riprog@hotmail.com>
2019-08-20 16:51:43 +0200

    Typescript Course Complete

--- Typescript The Complete Developer's Guide/features/metadata.ts
+++ Typescript The Complete Developer's Guide/features/metadata.ts
@@ -1,8 +1,40 @@
-import 'reflect-metadata';
+// import 'reflect-metadata';
 
-const plane = {
-  color: 'red'
-};
+// @controller
+// class Plane {
+//   color: string = 'red';
+
+//   @get('/login')
+//   fly(): void {
+//     console.log('vrrrrrr');
+//   }
+// }
+
+// function get(path: string){
+//   return function (target: Plane, key: string) {
+//     Reflect.defineMetadata('path', path, target, key);
+//   };
+// }
+
+// function controller(target: typeof Plane) {
+//   for (let key in target.prototype){
+//     const path = Reflect.getMetadata('path', target.prototype, key);
+//     const middleware = Reflect.getMetadata('middleware', target.prototype, key);
+
+//     router.get(path, middleware, target.prototype[key]);
+//   }
+// }
+
+
+
+
+
+
+
+
+// const plane = {
+//   color: 'red'
+// };
 
 // Metadata lets us attach little pieces of information to an object
 // Reflect.defineMetadata('note', 'hi there', plane);
@@ -14,8 +46,8 @@ const plane = {
 // console.log(note);
 // console.log(height);
 
-Reflect.defineMetadata('note', 'hi there', plane, 'color');
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
 
-const note = Reflect.getMetadata('note', plane, 'color');
+// const note = Reflect.getMetadata('note', plane, 'color');
 
-console.log(note);
+// console.log(note);
