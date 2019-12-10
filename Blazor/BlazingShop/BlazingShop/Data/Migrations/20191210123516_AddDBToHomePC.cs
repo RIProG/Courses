d4819a435fd84187852e8d7c64f46f14abf68711
RIProG <riprog@hotmail.com>
2019-12-10 13:59:56 +0100

    FromHomePc

---
+++ Blazor/BlazingShop/BlazingShop/Data/Migrations/20191210123516_AddDBToHomePC.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazingShop.Data.Migrations
+{
+    public partial class AddDBToHomePC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
